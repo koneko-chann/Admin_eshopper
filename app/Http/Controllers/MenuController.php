43f5decced4bf3d387d7e961dd10fbf50fdb75b3
tranngochuy0511 <tranngochuy20021105@gmail.com>
2023-11-02 02:25:02 +0700

    dfsadojbfsduyhfv

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Components\MenuRecursive;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+class MenuController extends Controller
+{
+    //
+    private $menuRecursive;
+    private $menu;
+
+    public function __construct(MenuRecursive $menuRecursive, Menu $menu)
+    {
+        $this->menuRecursive = $menuRecursive;
+        $this->menu = $menu;
+    }
+
+    public function index(): View
+    {
+        $menus=$this->menu->latest()->paginate(5);
+        return view('menus.index',compact('menus'));
+    }
+
+    public function create()
+    {
+        $optionSelect = $this->menuRecursive->menuRecursiveAdd();
+        return view('menus.add', compact('optionSelect'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->menu->create([
+            'name' => $request['name'],
+            'parent_id' => $request['parent_id'],
+            'slug' => Str::slug($request['name'], '-')
+        ]);
+        return redirect()->route('menus.index');
+    }
+
+    public function edit($id, Request $request)
+    {
+        $menu = $this->menu->find($id);
+        $htmlOption = $this->menuRecursive->menuRecursiveEdit($parentIdEdit = $menu['parent_id']);
+        return view('menus.edit', compact('menu', 'htmlOption'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->menu->find($id)->update([
+            'name' => $request['name'],
+            'parent_id' => $request['parent_id'],
+            'slug'=>Str::slug($request['name'],'-')
+        ]);
+        return redirect()->route('menus.index');
+
+    }
+public  function  delete($id){
+    $concat_string=null;
+    foreach ($this->menu->all() as $choosed){
+        if($choosed->parent_id ==$id){
+            $concat_string=$concat_string.$choosed->name.',';
+        }
+    }
+    $concat_string=rtrim($concat_string,',');
+    if(!empty($concat_string) || is_null($concat_string))
+        return redirect()->route('menus.index')->with('message','Không thể xóa vì có các menu con: '.$concat_string);
+    $finded= $this->menu->find($id)->delete();
+    return redirect()->route('menus.index');
+
+}
+}
